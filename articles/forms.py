37baeb106fa5f609577a4e073297c0d91a36994a
Xhevair Llakaj <xhllakaj@gmail.com>
2021-10-12 16:32:30 +0200

    adding crteate, update and delete buttons

--- articles/forms.py
+++ articles/forms.py
@@ -1,5 +1,5 @@
-from django import forms
 from django.contrib.auth import get_user_model
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.forms import fields
 from .models import Article
